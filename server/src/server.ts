fccb33d3eea4be6e228cd775d5e6b08d07f78c29
WagnerReis <wagnerreis1997@gmail.com>
2023-05-24 23:12:28 -0300

    aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,23 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
-import { memorisRoutes } from './routes/memories'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+
 app.register(cors, {
   origin: true,
+})
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
 
 app.register(jwt, {
@@ -17,7 +27,8 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
-app.register(memorisRoutes)
+app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
